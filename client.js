496a7460d00f950bf7de2b35f0702dba86c76f32
ncarvajalerker <36217235+ncarvajalerker@users.noreply.github.com>
2019-09-19 00:08:14 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,1 +1,38 @@
+// client-side js
+// run by the browser each time your view template is loaded
 
+console.log('hello world!');
+
+const movie = [
+  'Avengers: Endgame',
+  'A Beautiful Mind',
+  'Pay it Forward'
+];
+
+const movieList = document.getElementById('movie');
+const movieForm = document.forms[0];
+const movieInput = dreamsForm.elements['movie'];
+
+
+const appendNewMovie = function(movie) {
+  const newListItem = document.createElement('li');
+  newListItem.innerHTML = movie;
+  movieList.appendChild(newListItem);
+}
+
+
+movie.forEach( function(movie) {
+  appendNewMovie(movie);
+});
+
+
+movieForm.onsubmit = function(event) {
+  event.preventDefault();
+
+  movie.push(movieInput.value);
+  appendNewMovie(movieInput.value);
+
+
+  movieInput.value = '';
+  movieInput.focus();
+};
